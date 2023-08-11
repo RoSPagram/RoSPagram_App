3e08942aecfec295113c64e8a71ae9c5376b6470
rdg1029 <wcle3456@gmail.com>
2023-08-11 11:50:49 +0900

    Added constrants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const String clientId = "";
+  static const String appSecret = "";
+  static const String redirectUrl = "";
+  static const String scope = "user_profile";
+  static const String responseType = "code";
+  static const String url = "https://api.instagram.com/oauth/authorize?client_id=$clientId&redirect_uri=$redirectUrl&scope=$scope&response_type=$responseType";
+}
