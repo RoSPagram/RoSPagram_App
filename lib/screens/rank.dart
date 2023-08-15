73a3e34e999250caa84c4d71b0fad524d5c1e850
rdg1029 <wcle3456@gmail.com>
2023-08-15 10:40:41 +0900

    Added rank.dart

---
+++ lib/screens/rank.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../widgets/rank_header.dart';
+import '../widgets/rank_list_item.dart';
+
+class Rank extends StatefulWidget {
+  const Rank({super.key});
+
+  State<Rank> createState() => _RankState();
+}
+
+class _RankState extends State<Rank> {
+
+  static const List<String> dummyUserName = ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'UserName'];
+  static const List<String> dummyUserRank = ['Master', 'Master', 'Diamond', 'Diamond', 'Diamond', 'Diamond', 'Platinum', 'Platinum', 'Platinum', 'Platinum'];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RankHeader(
+          index: 10,
+          userName: 'UserName',
+          userRank: 'Platinum',
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            '🏆 TOP 10',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: dummyUserName.length,
+            itemBuilder: (BuildContext context, int index) {
+              return RankListItem(
+                index: index + 1,
+                userName: dummyUserName[index],
+                userRank: dummyUserRank[index],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
