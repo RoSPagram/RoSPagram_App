0843e72db5b51eb75725eeb320cf4654b8eb4025
rdg1029 <wcle3456@gmail.com>
2023-08-22 12:12:53 +0900

    Update main.dart : Init SharedPrefs & Apply provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './utilities/shared_prefs.dart';
 import './screens/sign_in.dart';
+import './providers/my_info.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPrefs().init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,14 +16,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RoSPagram',
-      theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => MyInfo(),
+      child: MaterialApp(
+        title: 'RoSPagram',
+        theme: ThemeData(
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          // useMaterial3: true,
+        ),
+        home: SignIn(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const SignIn(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
