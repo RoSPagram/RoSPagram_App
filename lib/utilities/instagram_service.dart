89182d7dcc60558df6a80aace1fd1451b88acdde
rdg1029 <wcle3456@gmail.com>
2023-08-23 18:54:09 +0900

    Update instagram_service.dart : Apply getUserProfileUrl()

--- lib/utilities/instagram_service.dart
+++ lib/utilities/instagram_service.dart
@@ -43,4 +43,13 @@ class InstagramService {
     final res = await http.get(uri);
     return jsonDecode(res.body);
   }
+
+  Future<String> getUserProfileImgUrl(String userName) async {
+    final uri = Uri.parse('https://www.instagram.com/$userName/?__a=1&__d=dis');
+    final res = await http.get(uri);
+    final resData = jsonDecode(res.body);
+    final imgUrl = resData['graphql']['user']['profile_pic_url'];
+
+    return imgUrl;
+  }
 }
