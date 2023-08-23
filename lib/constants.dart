2f526809a7e032a0ef748c3f4f2b6e838731ccf0
rdg1029 <wcle3456@gmail.com>
2023-08-23 19:00:45 +0900

    Update constants.dart : Apply dummy user data

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,6 +8,20 @@ const String INSTAGRAM_API_SCOPE = "user_profile";
 const String INSTAGRAM_API_RESPONSE_TYPE = "code";
 const String INSTAGRAM_API_URL = "https://api.instagram.com/oauth/authorize?client_id=$INSTAGRAM_API_CLIENT_ID&redirect_uri=$INSTAGRAM_API_REDIRECT_URL&scope=$INSTAGRAM_API_SCOPE&response_type=$INSTAGRAM_API_RESPONSE_TYPE";
 
+//Dummy user data
+const Map<String, dynamic> DUMMY_USER_DATA = {
+  '0': {'username': 'instagram', 'rank': 'Master'},
+  '1': {'username': 'cristiano', 'rank': 'Diamond'},
+  '2': {'username': 'leomessi', 'rank': 'Platinum'},
+  '3': {'username': 'selenagomez', 'rank': 'Gold'},
+  '4': {'username': 'kyliejenner', 'rank': 'Gold'},
+  '5': {'username': 'therock', 'rank': 'Silver'},
+  '6': {'username': 'arianagrande', 'rank': 'Silver'},
+  '7': {'username': 'kimkardashian', 'rank': 'Bronze'},
+  '8': {'username': 'beyonce', 'rank': 'Bronze'},
+  '9': {'username': 'khloekardashian', 'rank': 'Unranked'},
+};
+
 // Gradient colors by rank
 List<Color> rankColorGradient(String userRank) {
   switch(userRank) {
