7780601f8ed3f6b609c9b333ee64e180db6e2ab8
rdg1029 <wcle3456@gmail.com>
2023-08-27 21:56:44 +0900

    Update social.dart : Fetch test data from supabase

--- lib/screens/social.dart
+++ lib/screens/social.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import '../widgets/social_friend_list_item.dart';
-import '../constants.dart';
+import '../utilities/supabase_util.dart';
 
 class Social extends StatelessWidget {
   const Social({super.key});
+
+  Future<List<dynamic>> _fetch() async {
+    final List<dynamic> usersData = await supabase.from('top_ten').select('username, img_url, rank');
+    return usersData;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,39 +25,53 @@ class Social extends StatelessWidget {
               Tab(text: 'Request'),
             ],
           ),
-          Expanded(
-            child: TabBarView(
-              children: [
-                GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
+          FutureBuilder(
+            future: _fetch(),
+            builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+              if (snapshot.hasData) {
+                return Expanded(
+                  child: TabBarView(
+                    children: [
+                      GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                        ),
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return SocialFriendListItem(
+                            userName: snapshot.data?[index]['username'],
+                            imgUrl: snapshot.data?[index]['img_url'],
+                            userRank: snapshot.data?[index]['rank'],
+                          );
+                        },
+                      ),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.9,
+                        ),
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return SocialFriendListItem(
+                            isRequest: true,
+                            userName: snapshot.data?[index]['username'],
+                            imgUrl: snapshot.data?[index]['img_url'],
+                            userRank: snapshot.data?[index]['rank'],
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  itemCount: DUMMY_USER_DATA.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return SocialFriendListItem(
-                      userName: DUMMY_USER_DATA[index.toString()]['username'],
-                      userRank: DUMMY_USER_DATA[index.toString()]['rank'],
-                    );
-                  },
-                ),
-                GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.9,
-                  ),
-                  itemCount: DUMMY_USER_DATA.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return SocialFriendListItem(
-                      isRequest: true,
-                      userName: DUMMY_USER_DATA[index.toString()]['username'],
-                      userRank: DUMMY_USER_DATA[index.toString()]['rank'],
-                    );
-                  },
-                ),
-              ],
-            ),
+                );
+              }
+              else {
+                return Center(
+                  child: Text('Loading...'),
+                );
+              }
+            },
           ),
         ],
       ),
