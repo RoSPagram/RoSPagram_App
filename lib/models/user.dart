4cba67e24ad4b6d67c49d92135e46a50071d980c
rdg1029 <wcle3456@gmail.com>
2023-08-27 21:14:29 +0900

    Update user.dart : Add parameters

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,19 @@
 class User {
   String id;
   String username;
+  String img_url;
+  int rank;
+  int win;
+  int loss;
+  int draw;
 
   User({
     this.id = '',
     this.username = '',
+    this.img_url = '',
+    this.rank = 0,
+    this.win = 0,
+    this.loss = 0,
+    this.draw = 0,
   });
 }
