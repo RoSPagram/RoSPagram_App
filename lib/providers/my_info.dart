06fdb1c7064f9091f85a3e63263806c830b1eb0b
rdg1029 <wcle3456@gmail.com>
2023-09-03 19:49:34 +0900

    Update my_info.dart : Removed setters

--- lib/providers/my_info.dart
+++ lib/providers/my_info.dart
@@ -2,48 +2,5 @@ import 'package:flutter/material.dart';
 import '../models/user.dart';
 
 class MyInfo extends User with ChangeNotifier {
-  @override set id(String value) {
-    super.id = value;
-    notifyListeners();
-  }
 
-  @override set username(String value) {
-    super.username = value;
-    notifyListeners();
-  }
-
-  @override set img_url(String value) {
-    super.img_url = value;
-    notifyListeners();
-  }
-
-  @override set index(int value) {
-    super.index = value;
-    notifyListeners();
-  }
-
-  @override set rank(int value) {
-    super.rank = value;
-    notifyListeners();
-  }
-
-  @override set win(int value) {
-    super.win = value;
-    notifyListeners();
-  }
-
-  @override set loss(int value) {
-    super.loss = value;
-    notifyListeners();
-  }
-
-  @override set draw(int value) {
-    super.draw = value;
-    notifyListeners();
-  }
-
-  @override set score(double value) {
-    super.score = value;
-    notifyListeners();
-  }
 }
