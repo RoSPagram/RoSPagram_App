12e884cf0eadaf7d3c35d5b9311613061982b904
rdg1029 <wcle3456@gmail.com>
2023-09-03 20:09:20 +0900

    Update my_info.dart : Added override function

--- lib/providers/my_info.dart
+++ lib/providers/my_info.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/user.dart';
 
 class MyInfo extends User with ChangeNotifier {
-
+  @override
+  void notifyListeners() {
+    super.notifyListeners();
+  }
 }
