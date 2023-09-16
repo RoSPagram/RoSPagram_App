eade0d5efcbeca326b7e62148b70274481f8af76
rdg1029 <wcle3456@gmail.com>
2023-09-16 18:53:36 +0900

    Update ranking_data.dart : Edit fetch() & Separate functions

--- lib/providers/ranking_data.dart
+++ lib/providers/ranking_data.dart
@@ -3,16 +3,29 @@ import '../utilities/supabase_util.dart';
 
 class RankingData extends ChangeNotifier {
   List<dynamic> list = [];
+  int rankedUsersCount = 0;
 
   @override
   void notifyListeners() {
     super.notifyListeners();
   }
 
-  void fetch() {
+  void fetchTopten() {
     supabase.from('top_ten').select('index, id, username, img_url, rank').then((value) {
       list = value;
       notifyListeners();
     });
+  }
+  
+  void fetchRankedUsersCount() {
+    supabase.rpc('get_ranked_users_count').then((value) {
+      rankedUsersCount = value;
+      notifyListeners();
+    });
+  }
+
+  void fetch() {
+    fetchTopten();
+    fetchRankedUsersCount();
   }
 }
