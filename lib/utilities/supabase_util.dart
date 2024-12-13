63833d91dae9afd0e8714becca8bd0acc50a434f
rdg1029 <wcle3456@gmail.com>
2024-12-13 22:13:04 +0900

    Update supabase_util.dart : Apply env

--- lib/utilities/supabase_util.dart
+++ lib/utilities/supabase_util.dart
@@ -1,11 +1,12 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final supabase = Supabase.instance.client;
 
 void initSupabase() async {
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: dotenv.env['SUPABASE_URL'] as String,
+    anonKey: dotenv.env['SUPABASE_ANON'] as String,
   );
 }
 
