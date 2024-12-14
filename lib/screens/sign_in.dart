448dfca50888c155759a7b2ae493937ed751a61f
rdg1029 <wcle3456@gmail.com>
2024-12-14 10:57:32 +0900

    Update sign_in.dart : Disable instagram sign in & Apply guest login

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 import '../providers/my_info.dart';
 import '../utilities/shared_prefs.dart';
 import '../utilities/instagram_service.dart';
 import '../utilities/supabase_util.dart';
+import '../utilities/username_generator.dart';
 import './auth_webview.dart';
 import './main_screen.dart';
 
@@ -11,25 +13,33 @@ class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   Future<bool> _fetch(BuildContext context) async {
-    String? oldToken = SharedPrefs.instance.getString('user_token');
-    if (oldToken == null) return false;
+    // String? oldToken = SharedPrefs.instance.getString('user_token');
+    // if (oldToken == null) return false;
+    //
+    // final newToken = await InstagramService().refreshUserToken(oldToken);
+    // final userProfile = await InstagramService().getUserInfo(newToken);
+    // if (userProfile['id'] == null) return false;
+    //
+    // final String? userImgUrl = await InstagramService().getUserProfileImgUrl(userProfile['username']);
 
-    final newToken = await InstagramService().refreshUserToken(oldToken);
-    final userProfile = await InstagramService().getUserInfo(newToken);
-    if (userProfile['id'] == null) return false;
+    String? storedUUID = SharedPrefs.instance.getString('uuid');
+    if (storedUUID == null) return false;
 
-    final String? userImgUrl = await InstagramService().getUserProfileImgUrl(userProfile['username']);
-    final userData = await supabase.rpc('get_user_data', params: {'user_id': userProfile['id']});
+    // final userData = await supabase.rpc('get_user_data', params: {'user_id': userProfile['id']});
+    final userData = await supabase.rpc('get_user_data', params: {'user_id': storedUUID});
     if (userData.length == 0) return false;
 
-    await supabase.from('users').update({
-      'username': userProfile['username'],
-      'img_url': userImgUrl ?? userData[0]['img_url'],
-    }).eq('id', userProfile['id']);
+    // await supabase.from('users').update({
+    //   'username': userProfile['username'],
+    //   'img_url': userImgUrl ?? userData[0]['img_url'],
+    // }).eq('id', userProfile['id']);
 
-    context.read<MyInfo>().id = userProfile['id'];
-    context.read<MyInfo>().username = userProfile['username'];
-    context.read<MyInfo>().img_url = userImgUrl ?? userData[0]['img_url'];
+    // context.read<MyInfo>().id = userProfile['id'];
+    // context.read<MyInfo>().username = userProfile['username'];
+    // context.read<MyInfo>().img_url = userImgUrl ?? userData[0]['img_url'];
+    context.read<MyInfo>().id = userData[0]['id'];
+    context.read<MyInfo>().username = userData[0]['username'];
+    context.read<MyInfo>().img_url = userData[0]['img_url'];
     context.read<MyInfo>().index = userData[0]['index'];
     context.read<MyInfo>().win = userData[0]['win'];
     context.read<MyInfo>().loss = userData[0]['loss'];
@@ -51,9 +61,33 @@ class SignIn extends StatelessWidget {
 
     FilledButton signInButton = FilledButton(
       style: FilledButton.styleFrom(backgroundColor: Color(0xff000000)),
-      child: Text('Sign in with Instagram'),
+      child: Text('Sign in'),
       onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => AuthWebView()));
+        // Navigator.push(context, MaterialPageRoute(builder: (context) => AuthWebView()));
+      },
+    );
+
+    FilledButton guestButton = FilledButton(
+      style: FilledButton.styleFrom(backgroundColor: Color(0xffdfdfdf), foregroundColor: Color(0xff000000)),
+      child: Text('Start as Guest'),
+      onPressed: () async {
+        final newUUID = Uuid().v4();
+        final newUserName = getRandomName();
+        //https://www.dicebear.com/styles/thumbs/
+        final newImgUrl = 'https://api.dicebear.com/9.x/thumbs/png?seed=$newUUID&scale=75';
+        context.read<MyInfo>().id = newUUID;
+        context.read<MyInfo>().username = newUserName;
+        context.read<MyInfo>().img_url = newImgUrl;
+
+        SharedPrefs.instance.setString('uuid', newUUID);
+
+        await supabase.from('users').insert({
+          'id': newUUID,
+          'username': newUserName,
+          'img_url': newImgUrl
+        });
+
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => MainScreen()), (route) => false);
       },
     );
 
@@ -128,7 +162,8 @@ class SignIn extends StatelessWidget {
                         Future.microtask(() => Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => MainScreen()), (route) => false));
                         return loadingText;
                       }
-                      else return signInButton;
+                      else return guestButton;
+                      // else return signInButton;
                     }
                     else return loadingText;
                   },
