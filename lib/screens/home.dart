25b43b11978a06e44314fed2263e59be9ce41e7a
rdg1029 <wcle3456@gmail.com>
2024-12-19 15:35:47 +0900

    Update home.dart : Apply token_view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../widgets/profile_header.dart';
+import '../widgets/token_view.dart';
 import '../widgets/win_loss_record.dart';
 import '../providers/my_info.dart';
 import './user_profile.dart';
-import './play.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -22,46 +22,7 @@ class Home extends StatelessWidget {
             );
           },
         ),
-        WinLossRecord(
-          win: context.watch<MyInfo>().win,
-          loss: context.watch<MyInfo>().loss,
-          draw: context.watch<MyInfo>().draw,
-          padding: EdgeInsets.all(16),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Play(isRequest: true))
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.all(32),
-            backgroundColor: Colors.deepOrange,
-          ),
-          child: Column(
-            children: [
-              Text(
-                'Start game',
-                style: TextStyle(
-                  fontSize: 32,
-                ),
-              ),
-              Text(
-                '👤',
-                style: TextStyle(
-                  fontSize: 32,
-                ),
-              ),
-              Text(
-                'with a random user',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ],
-          ),
-        ),
+        TokenView(userId: context.read<MyInfo>().id),
         Text('Your ID : ${context.watch<MyInfo>().id}'),
       ],
     );
