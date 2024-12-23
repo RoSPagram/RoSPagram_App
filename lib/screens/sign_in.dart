928b32904fb16c4cb86c5cfbc9c9fbcd5411e459
rdg1029 <wcle3456@gmail.com>
2024-12-23 09:47:00 +0900

    Update sign_in.dart : Fix update user data issue

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -8,10 +8,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../providers/my_info.dart';
 import '../utilities/avatar_util.dart';
 import '../utilities/shared_prefs.dart';
-import '../utilities/instagram_service.dart';
 import '../utilities/supabase_util.dart';
 import '../utilities/username_generator.dart';
-import './auth_webview.dart';
 import './main_screen.dart';
 
 class SignIn extends StatelessWidget {
@@ -27,7 +25,7 @@ class SignIn extends StatelessWidget {
     final Map<String, dynamic> updates = {};
 
     String? fcmToken = SharedPrefs.instance.getString('fcm_token');
-    if (userData[0]['fcm_token'] == null) {
+    if (userData[0]['fcm_token'] == null || userData[0]['fcm_token'] != fcmToken) {
       updates['fcm_token'] = fcmToken;
     }
 
@@ -42,19 +40,18 @@ class SignIn extends StatelessWidget {
       updates['lang'] = currentLang;
     }
 
-    NTP.now().then((time) async {
-      final date = DateFormat('yyyy-MM-dd').format(time);
-      if (userData[0]['last_login'] == date) return;
+    final time = await NTP.now();
+    final date = DateFormat('yyyy-MM-dd').format(time);
+    if (userData[0]['last_login'] != date) {
       updates['last_login'] = date;
-    });
+    }
 
     if (updates.isNotEmpty) {
-      await supabase.from('users').update(updates).eq('id', storedUUID);
+      supabase.from('users').update(updates).eq('id', storedUUID).then((_) {});
     }
 
     context.read<MyInfo>().id = userData[0]['id'];
     context.read<MyInfo>().username = userData[0]['username'];
-    context.read<MyInfo>().img_url = userData[0]['img_url'];
     context.read<MyInfo>().avatarData = userData[0]['avatar'] ?? updates['avatar'];
     context.read<MyInfo>().index = userData[0]['index'];
     context.read<MyInfo>().win = userData[0]['win'];
@@ -91,21 +88,28 @@ class SignIn extends StatelessWidget {
       onPressed: () async {
         final newUUID = Uuid().v4();
         final newUserName = getRandomName();
-        //https://www.dicebear.com/styles/thumbs/
-        //https://github.com/Ivanmtta/anonymous-animals-api
-        final newImgUrl = 'https://api.dicebear.com/9.x/thumbs/png?seed=$newUUID&scale=75';
+        Avatar avatar = new Avatar();
+        avatar.applyRandom();
         context.read<MyInfo>().id = newUUID;
         context.read<MyInfo>().username = newUserName;
-        context.read<MyInfo>().img_url = newImgUrl;
+        context.read<MyInfo>().avatarData = jsonEncode(avatar.toJSON());
+        context.read<MyInfo>().fcm_token = SharedPrefs.instance.getString('fcm_token')!;
 
         SharedPrefs.instance.setString('uuid', newUUID);
+
+        final time = await NTP.now();
+        final date = DateFormat('yyyy-MM-dd').format(time);
 
         await supabase.from('users').insert({
           'id': newUUID,
           'username': newUserName,
-          'img_url': newImgUrl,
-          'fcm_token': SharedPrefs.instance.getString('fcm_token')
+          'avatar': context.read<MyInfo>().avatarData,
+          'fcm_token': context.read<MyInfo>().fcm_token,
+          'last_login': date,
+          'lang': Localizations.localeOf(context).languageCode,
         });
+
+        context.read<MyInfo>().notifyListeners();
 
         Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => MainScreen()), (route) => false);
       },
