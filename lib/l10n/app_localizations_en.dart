b43920182b1729b7a5aebda98d0be2958c4761e9
rdg1029 <wcle3456@gmail.com>
2025-05-23 15:29:27 +0900

    Apply app localization files

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,259 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get yes => 'Yes';
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get confirm => 'Confirm';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get update => 'Update';
+
+  @override
+  String get finger => 'Finger';
+
+  @override
+  String get ranking => 'RANKING';
+
+  @override
+  String get top => 'TOP';
+
+  @override
+  String get update_required => 'Update required.';
+
+  @override
+  String get sign_in_title => 'Finger\nGrowth';
+
+  @override
+  String get sign_in_start => 'Get Started';
+
+  @override
+  String get main_screen_navbar_home => 'Home';
+
+  @override
+  String get main_screen_navbar_match => 'Match';
+
+  @override
+  String get main_screen_navbar_rank => 'Rank';
+
+  @override
+  String get main_screen_navbar_social => 'Social';
+
+  @override
+  String get main_screen_navbar_shop => 'Shop';
+
+  @override
+  String get token_view_msg_no => 'No tokens!';
+
+  @override
+  String get play_btn_text => 'Play';
+
+  @override
+  String get play_btn_dialog_title => 'Play';
+
+  @override
+  String get play_btn_dialog_content => 'Start with a token?';
+
+  @override
+  String get match_title => 'Game Request';
+
+  @override
+  String get match_tab_from => 'From';
+
+  @override
+  String get match_tab_to => 'To';
+
+  @override
+  String get match_no => 'No matches';
+
+  @override
+  String get match_item_from_desc => 'Touch to Accept';
+
+  @override
+  String get match_item_to_desc_cancel => 'Touch to Cancel';
+
+  @override
+  String get match_item_to_desc_show => 'Touch to show result';
+
+  @override
+  String get match_dialog_cancel_title => 'Cancel Match';
+
+  @override
+  String get match_dialog_cancel_content => 'Are you cancel this game?';
+
+  @override
+  String get shop_item_change_name => 'Change name';
+
+  @override
+  String get shop_item_change_random_avatar => 'Random avatar';
+
+  @override
+  String get shop_item_face_position => 'Face Position';
+
+  @override
+  String get shop_item_body_position => 'Body Position';
+
+  @override
+  String get shop_item_body_color => 'Body Color';
+
+  @override
+  String get shop_item_background_color => 'Background Color';
+
+  @override
+  String get shop_item_cheek_color => 'Cheek Color';
+
+  @override
+  String get shop_change_alert_title => 'Change Avatar';
+
+  @override
+  String get shop_change_alert_msg => 'Change it?';
+
+  @override
+  String get shop_buy_alert_msg => 'Buy it?';
+
+  @override
+  String get shop_not_enough_msg => 'Not enough gems!';
+
+  @override
+  String get shop_avatar_change_msg => 'Avatar changed.';
+
+  @override
+  String get shop_name_change_msg => 'Name changed.';
+
+  @override
+  String get editor_text_red => 'Red';
+
+  @override
+  String get editor_text_green => 'Green';
+
+  @override
+  String get editor_text_blue => 'Blue';
+
+  @override
+  String get editor_text_rotate => 'Rotate';
+
+  @override
+  String get editor_name_title => 'Change Name';
+
+  @override
+  String get editor_name_label => 'Type name';
+
+  @override
+  String get editor_name_hint => 'Enter the name you want to change.';
+
+  @override
+  String get editor_name_invalid_over_1 => 'Alphabet, Number: Up to 18 characters';
+
+  @override
+  String get editor_name_invalid_over_2 => 'Others: Up to 9 characters';
+
+  @override
+  String get editor_name_invalid_short_1 => 'Alphabet, Number: 3 or more characters';
+
+  @override
+  String get editor_name_invalid_short_2 => 'Others: 2 or more characters';
+
+  @override
+  String get reward_btn_text => 'Get Gems by Watching Ads';
+
+  @override
+  String get reward_snackbar_msg => 'Watch Ad now & Get';
+
+  @override
+  String get reward_snackbar_action => 'Watch Ad';
+
+  @override
+  String get reward_dialog_title => 'Get Gems';
+
+  @override
+  String get reward_dialog_content_watch => 'Watch Ad';
+
+  @override
+  String get reward_dialog_content_click => 'Click Ad';
+
+  @override
+  String get reward_dialog_watch_title => 'Reward: Watched Ad';
+
+  @override
+  String get reward_dialog_click_title => 'Reward: Clicked Ad';
+
+  @override
+  String get play_no_users => 'User not found.';
+
+  @override
+  String get play_btn_start => 'Start Game';
+
+  @override
+  String get play_btn_reload => 'Find Again';
+
+  @override
+  String get play_select => 'Choose your hand';
+
+  @override
+  String get play_dialog_exit_title => 'Exit';
+
+  @override
+  String get play_dialog_exit_content => 'Are you exit this game?';
+
+  @override
+  String get play_dialog_exit_content_desc => 'Used tokens cannot be recovered';
+
+  @override
+  String get play_dialog_already_title => 'Already in Progress';
+
+  @override
+  String get play_dialog_already_content => 'You are already playing with this user';
+
+  @override
+  String get play_dialog_already_action => 'Find others';
+
+  @override
+  String get result_text_win => 'YOU WIN';
+
+  @override
+  String get result_text_lose => 'YOU LOSE';
+
+  @override
+  String get result_text_draw => 'DRAW';
+
+  @override
+  String get win_loss_record_win => 'WIN';
+
+  @override
+  String get win_loss_record_lose => 'LOSE';
+
+  @override
+  String get win_loss_record_draw => 'DRAW';
+
+  @override
+  String get push_msg_body_match_req => 'Match request';
+
+  @override
+  String get push_msg_body_result_win => 'You won!';
+
+  @override
+  String get push_msg_body_result_loss => 'You lost..';
+
+  @override
+  String get push_msg_body_result_draw => 'Draw';
+
+  @override
+  String get test_msg => 'English test msg';
+}
